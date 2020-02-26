f24398239c52601d078a25b363c06254870e2b0a
Yvonne K <yvonneking017@gmail.com>
2020-02-26 15:13:51 -0600

    tweaked summing function and updated while loop)

--- checkbook_app.py
+++ checkbook_app.py
@@ -1,9 +1,7 @@
-with open("transactions.txt", "r") as f:
-    balances = f.readlines()
+
 
 #global variables - putting at beginning to ensure helper functions and print statements run
 
-balance_transactions_list = balances
 
 greeting = "*~ Wilkommen, welcome to your terminal checkbook ~*"
 
@@ -30,22 +28,24 @@ print(menu_options)
 print("\n")
 
 #helper functions
-def remove_new_line_characters(some_list = balance_transactions_list):
+def remove_new_line_characters(some_list):
     new_list = []
     for item in some_list:
         new_list.append(item.replace("\n", ""))
     return new_list
 
-def convert_item_to_float(some_list = balance_transactions_list):
+def convert_item_to_float(some_list):
     new_list = []
     for item in some_list:
         new_list.append(float(item))
     return new_list
 
-def sum_of_all_transactions(some_list = balance_transactions_list):
-    return sum(convert_item_to_float((remove_new_line_characters(some_list))))
+def sum_of_all_transactions()
+with open("transactions.txt", "r") as f:
+    balances = f.readlines()
+    return sum(convert_item_to_float((remove_new_line_characters(balances))))
 
-def view_account_balance(some_list = balance_transactions_list):
+def view_account_balance(some_list):
     return sum_of_all_transactions(some_list)
 
 def add_a_deposit_to_file(amount):
@@ -56,13 +56,14 @@ def add_a_withdrawel_to_file(amount):
     with open("transactions.txt", "a") as f:
         f.write(amount)
 
-
+def convert_withdrawel_input_to_negative(amount):
+    return amount * -1
 
 while True:
     user_input_for_menu_selection = input("How would you like to proceed?")
     user_input_for_menu_selection = int(user_input_for_menu_selection)
     if user_input_for_menu_selection == 1:
-        print("Your current balance is $", str(view_account_balance()))
+        print("Your current balance is $", str(sum_of_all_transactions()))
         print("\n")
         print(menu_header)
         print(menu_options)
@@ -70,7 +71,12 @@ while True:
     elif user_input_for_menu_selection == 2:
         deposit_amount = input("How much would you like to deposit?")
         add_a_deposit_to_file(deposit_amount + "\n")
-        print("Your new blance is $", str(view_account_balance() + float(deposit_amount)))
+        print("Your new blance is $", str(sum_of_all_transactions()))
+    elif user_input_for_menu_selection == 3:
+        withdraw_amount = input("How much would you like to withdraw?")
+        withdraw_amount = convert_withdrawel_input_to_negative(float(withdraw_amount))
+        add_a_withdrawel_to_file(str(withdraw_amount))
+        print("Your new blance is $", str(sum_of_all_transactions()))
     else:
         print("\n")
         print(goodbye)
